6259b3cb15b3fbb5b0d022b79d9f84858f54e1ab
Sebastian Scaini <sebastianscaini@gmail.com>
2022-11-07 12:42:13 -0500

    Made Single-Target Copying

--- FFXIVLocalCharacterSettingsTransferTool/Program.cs
+++ FFXIVLocalCharacterSettingsTransferTool/Program.cs
@@ -8,6 +8,7 @@ namespace FFXIVLocalCharacterSettingsTransferTool
         {
             string FFXIVDataPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn", "FFXIV.cfg");
             string[] FFXIVData = System.IO.File.ReadAllLines(FFXIVDataPath);
+            string fileName = "";
 
             Console.WriteLine("FFXIV Local Character Settings Transfer Tool");
             Console.WriteLine("By Sebastian Scaini");
@@ -24,13 +25,18 @@ namespace FFXIVLocalCharacterSettingsTransferTool
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
 
+            FFXIVDataPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn", "FFXIV.cfg");
+            FFXIVData = System.IO.File.ReadAllLines(FFXIVDataPath);
+
             string Line1 = FFXIVData[12].Replace("LastLogin0	", "");
             string Line2 = FFXIVData[13].Replace("LastLogin1	", "");
 
             int Line1Int = int.Parse(Line1);
             int Line2Int = int.Parse(Line2);
 
-            string chrPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn/" + ("CHR" + "00" + Line2Int.ToString("X") + Line1Int.ToString("X")));
+            string chrPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn/" + ("FFXIV_CHR" + "00" + Line2Int.ToString("X") + Line1Int.ToString("X")));
+            string sourcePath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn/" + ("FFXIV_CHR" + "00" + Line2Int.ToString("X") + Line1Int.ToString("X")));
+
             Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Found Chararacter: " + ("CHR" + "00" + Line2Int.ToString("X") + Line1Int.ToString("X")));
@@ -52,9 +58,13 @@ namespace FFXIVLocalCharacterSettingsTransferTool
             int Line1Int2 = int.Parse(Line12);
             int Line2Int2 = int.Parse(Line22);
 
-            string chrPath2 = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn/" + ("CHR" + "00" + Line2Int2.ToString("X")+ Line1Int2.ToString("X")));
+            string chrPath2 = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn/" + ("FFXIV_CHR" + "00" + Line2Int2.ToString("X")+ Line1Int2.ToString("X")));
+            string targetPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn/" + ("FFXIV_CHR" + "00" + Line2Int2.ToString("X")+ Line1Int2.ToString("X")));
 
-            if(chrPath == chrPath2)
+            string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
+            string destFile = System.IO.Path.Combine(targetPath, fileName);
+
+            if (chrPath == chrPath2)
             {
                 Console.WriteLine("");
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -75,12 +85,35 @@ namespace FFXIVLocalCharacterSettingsTransferTool
                 Console.WriteLine("The tool is about to overwrite the target character's settings.");
                 Console.WriteLine("Press any key twice to confirm. Close the tool to cancel.");
                 Console.ForegroundColor = ConsoleColor.White;
-            }
+                Console.ReadKey();
+                Console.ReadKey();
 
-            //Console.WriteLine("");
-            /*Console.WriteLine(Line1Int.ToString("X"));
-            Console.WriteLine("00" + Line2Int.ToString("X"));*/
-            //Console.WriteLine(chrPath2);
+                if (System.IO.Directory.Exists(sourcePath))
+                {
+                    string[] files = System.IO.Directory.GetFiles(sourcePath);
+
+                    foreach (string s in files)
+                    {
+                        fileName = System.IO.Path.GetFileName(s);
+                        destFile = System.IO.Path.Combine(targetPath, fileName);
+                        System.IO.File.Copy(s, destFile, true);
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Copied " + s);
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("");
+                    Console.WriteLine("Copying complete!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Target invalid. Aborting copy.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+            }
 
             Console.WriteLine("");
             Console.WriteLine("Press any key to exit the tool.");

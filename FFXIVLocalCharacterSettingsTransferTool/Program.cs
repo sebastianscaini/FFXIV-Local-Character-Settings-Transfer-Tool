ae048b7df95824463c5d848fdcb849d3bcd5ffba
Sebastian Scaini <sebastianscaini@gmail.com>
2022-11-07 11:22:35 -0500

    Fixed Character Detection

--- FFXIVLocalCharacterSettingsTransferTool/Program.cs
+++ FFXIVLocalCharacterSettingsTransferTool/Program.cs
@@ -38,9 +38,14 @@ namespace FFXIVLocalCharacterSettingsTransferTool
 
             Console.WriteLine("");
             Console.WriteLine("Login with the character that you wish to copy the settings to, then log out and close the game.");
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("If you wish to copy these settings to all other character, continue without logging in to another character.");
+            Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
+
+            FFXIVDataPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn", "FFXIV.cfg");
+            FFXIVData = System.IO.File.ReadAllLines(FFXIVDataPath);
 
             string Line12 = FFXIVData[12].Replace("LastLogin0	", "");
             string Line22 = FFXIVData[13].Replace("LastLogin1	", "");
@@ -48,10 +53,24 @@ namespace FFXIVLocalCharacterSettingsTransferTool
             int Line2Int2 = int.Parse(Line22);
 
             string chrPath2 = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn/" + ("CHR" + "00" + Line2Int2.ToString("X")+ Line1Int2.ToString("X")));
-            Console.WriteLine("");
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Found Chararacter: " + ("CHR" + "00" + Line2Int2.ToString("X") + Line1Int2.ToString("X")));
-            Console.ForegroundColor = ConsoleColor.White;
+
+            if(chrPath == chrPath2)
+            {
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("The tool is about to overwrite all of your character settings with the selected character's settings.");
+                Console.WriteLine("Press any key twice to confirm. Close the tool to cancel.");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.ReadKey();
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Found Chararacter: " + ("CHR" + "00" + Line2Int2.ToString("X") + Line1Int2.ToString("X")));
+                Console.ForegroundColor = ConsoleColor.White;
+            }
 
             //Console.WriteLine("");
             /*Console.WriteLine(Line1Int.ToString("X"));

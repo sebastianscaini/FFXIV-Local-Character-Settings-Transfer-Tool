7c77d711f5aac05820d8bfb11f97dc5d9da35cdb
Sebastian Scaini <sebastianscaini@gmail.com>
2022-11-07 11:13:08 -0500

    Fixed Character Detection Hex Output

--- FFXIVLocalCharacterSettingsTransferTool/Program.cs
+++ FFXIVLocalCharacterSettingsTransferTool/Program.cs
@@ -30,32 +30,33 @@ namespace FFXIVLocalCharacterSettingsTransferTool
             int Line1Int = int.Parse(Line1);
             int Line2Int = int.Parse(Line2);
 
-            string chrPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn/" + ("CHR" + Line1Int.ToString("X") + "00" + Line2Int.ToString("X")));
+            string chrPath = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn/" + ("CHR" + "00" + Line2Int.ToString("X") + Line1Int.ToString("X")));
             Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Found Chararacter: " + ("CHR" + Line1Int.ToString("X") + "00" + Line2Int.ToString("X")));
+            Console.WriteLine("Found Chararacter: " + ("CHR" + "00" + Line2Int.ToString("X") + Line1Int.ToString("X")));
             Console.ForegroundColor = ConsoleColor.White;
 
             Console.WriteLine("");
             Console.WriteLine("Login with the character that you wish to copy the settings to, then log out and close the game.");
+            Console.WriteLine("If you wish to copy these settings to all other character, continue without logging in to another character.");
             Console.WriteLine("Press any key to continue.");
             Console.ReadKey();
 
-            Line1 = FFXIVData[12].Replace("LastLogin0	", "");
-            Line2 = FFXIVData[13].Replace("LastLogin1	", "");
-            Line1Int = int.Parse(Line1);
-            Line2Int = int.Parse(Line2);
+            string Line12 = FFXIVData[12].Replace("LastLogin0	", "");
+            string Line22 = FFXIVData[13].Replace("LastLogin1	", "");
+            int Line1Int2 = int.Parse(Line12);
+            int Line2Int2 = int.Parse(Line22);
 
-            string chrPath2 = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn/" + ("CHR" + Line1Int.ToString("X") + "00" + Line2Int.ToString("X")));
+            string chrPath2 = System.IO.Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "/My Games/FINAL FANTASY XIV - A Realm Reborn/" + ("CHR" + "00" + Line2Int2.ToString("X")+ Line1Int2.ToString("X")));
             Console.WriteLine("");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Found Chararacter: " + ("CHR" + Line1Int.ToString("X") + "00" + Line2Int.ToString("X")));
+            Console.WriteLine("Found Chararacter: " + ("CHR" + "00" + Line2Int2.ToString("X") + Line1Int2.ToString("X")));
             Console.ForegroundColor = ConsoleColor.White;
 
-            Console.WriteLine("");
+            //Console.WriteLine("");
             /*Console.WriteLine(Line1Int.ToString("X"));
             Console.WriteLine("00" + Line2Int.ToString("X"));*/
-            Console.WriteLine(chrPath2);
+            //Console.WriteLine(chrPath2);
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
